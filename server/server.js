75e1ea556549dc7eb903a1cfb29db8d01b41625e
Shreya <shreyamasta99@gmail.com>
2024-12-30 19:16:33 +0530

    cors allowed all sites

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3001", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
